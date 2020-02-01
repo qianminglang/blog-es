c76d9259fa9e0fde3be8c268cc223e8a02f32206
qml <asiainfo.com>
2020-02-01 12:19:48 +0800

    Initial commit

---
+++ src/main/java/com/imooc/bloges/BlogEsApplication.java
@@ -0,0 +1,13 @@
+package com.imooc.bloges;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogEsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogEsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/imooc/bloges/BlogEsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imooc.bloges;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogEsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
